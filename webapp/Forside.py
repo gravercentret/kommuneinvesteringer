488861604f8d531341b69aaf8cffb7f387d47b70
Martin Dreyer <martinchristiandreyer@gmail.com>
2025-12-05 16:47:06 +0100

    newsletter update

--- webapp/Forside.py
+++ webapp/Forside.py
@@ -25,7 +25,6 @@ from utils.data_processing import (
 )
 from utils.plots import create_pie_chart
 from config import set_pandas_options, set_streamlit_options
-from utils.newsletter_popup import newsletter_popup
 
 # Apply the settings
 set_pandas_options()
@@ -42,8 +41,48 @@ st.logo(
     "webapp/images/GC_png_oneline_lockup_Outline_Blaa_RGB.png", link="https://gravercentret.dk/"
 )
 
+import time
+from datetime import timedelta, datetime
 
-newsletter_popup
+from streamlit_cookies_controller import CookieController
+
+
+controller = CookieController(key="newsletter_popup")
+
+
+def _close_popup():
+    expire_at = datetime.now() + timedelta(days=30)
+    controller.set("newsletter_popup_closed", "true", expires=expire_at)
+
+
+def newsletter_popup():
+    closed = controller.get("newsletter_popup_closed")
+    if closed == "true":
+        return
+
+    container = st.container(
+        width=500,
+        border=True,
+    )
+    with container:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image("webapp/images/newsletter.png")
+        with col2:
+            st.markdown(
+                """
+                ### Tilmeld dig Gravercentrets nyhedsbrev
+                Få inspiration, tips, viden og researchpakker og læs, hvordan vi hjælper medier.
+
+                Vi udsender nyhedsbrevet nogle få gange hver måned. Du kan altid afmelde det igen.
+                """
+            )
+        with st.container(horizontal=True):
+            st.link_button("Tilmeld nyhedsbrev", url="https://gravercentret.dk/nyhedsbrev/")
+            close = st.button("Luk", on_click=_close_popup)
+
+
+newsletter_popup()
 # Title of the app
 st.title("Kommunernes og regionernes investeringer")
 
